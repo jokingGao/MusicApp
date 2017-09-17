89f0e286816ae77f315548788d68f5cbf5af004b
JIANQIN GAO <jianqingao@JIANQINsMacBook.fios-router.home>
2017-09-17 15:55:17 -0400

    react router configuration

--- app/index.js
+++ app/index.js
@@ -2,8 +2,25 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
+//import react router
+import { HashRouter, Router, Route, hashHistory, Link } from 'react-router-dom';
+
+
+//import self defined components
+import Home from './pages/home';
+import Login from './pages/login';
+
 render((
-    <div>hello, world!</div>
+    <HashRouter>
+        <div>
+            <ul>
+                <li><Link to='/' >Home</Link></li>
+                <li><Link to='/login'>Login Page</Link></li>
+            </ul>
+            <Route exact path='/' component={Home} />
+            <Route path='/login' component={Login} />
+        </div>
+    </HashRouter>
 ), document.getElementById('content'));
 
 
---
+++ app/pages/login.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+
+class Login extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div> This is Login page </div>
+        )
+    }
+}
+
+export default Login;
